d55e69cfc10da5797e5956a147fc94b07ee6f5d8
dotenvsolutions <dotenvsolutions@gmail.com>
2023-11-30 15:16:07 -0500

    Agregado imagen descriptiva

--- pacientes/patient_tracker/src/components/Pacientes.jsx
+++ pacientes/patient_tracker/src/components/Pacientes.jsx
@@ -1,21 +1,27 @@
 const Pacientes = ({ paciente }) => {
   const { nombre, dueno, sintoma } = paciente
   return (
-    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
-      <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
-        <span className="font-normal normal-case ">{nombre}</span>
-      </p>
-      <p className="font-bold mb-3 text-gray-700 uppercase">Dueño: {''}
-        <span className="font-normal normal-case ">{dueno}</span>
-      </p>
-      <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
-        <span className="font-normal normal-case ">{sintoma}</span>
-      </p>
+    <>
+      <div class="flex flex-col md:flex-row justify-center items-center">
+        <div class="w-full md:w-400 px-4 py-6">
+          <img src="https://picsum.photos/400/400" alt="Imagen descriptiva" />
+        </div>
+        <div class="w-full md:w-200 px-4 py-6 mt-6 md:mt-0">
+          <h2 class="text-2xl font-semibold">Nombres {''}</h2>
+          <p class="text-lg">{nombre}</p>
+          <h2 class="text-2xl font-semibold mt-4">Apellidos {''}</h2>
+          <p class="text-lg">{dueno}</p>
+          <h2 class="text-2xl font-semibold mt-4">Sintoma {''}</h2>
+          <p class="text-lg">{sintoma}</p>
+        </div>
+      </div>
       <div className="flex justify-between mt-10">
         <button type="button" className="bg-indigo-600 hover:bg-indigo-700 py-2 px-10 uppercase font-bold rounded-lg" >Editar</button>
         <button type="button" className="bg-red-600 hover:bg-red-700 py-2 px-10 uppercase font-bold rounded-lg" >Eliminar</button>
       </div>
-    </div>
+    </>
+
+
   )
 }
 
