4f6fb590af005c01330ba03daa217edb52c5ec97
dotenvsolutions <dotenvsolutions@gmail.com>
2023-11-30 15:05:09 -0500

    Cambios en los componentes

--- pacientes/patient_tracker/src/components/Formulario.jsx
+++ pacientes/patient_tracker/src/components/Formulario.jsx
@@ -1,25 +1,48 @@
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
 
 
-const Formulario = () => {
+const Formulario = ({ pacientes, setPacientes }) => {
+  const [nombre, setNombre] = useState("")
+  const [dueno, setDueno] = useState("")
+  const [sintoma, setSintomas] = useState("")
+  const getIdentify = () => {
+    const fecha = Date.now().toString(36)
+    const number = Math.random().toString(36).substr(2)
+    return fecha + number
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const objPatient = {
+      nombre,
+      dueno,
+      sintoma,
+      id: getIdentify()
+    }
+
+    setPacientes([...pacientes, objPatient])
+    setNombre('')
+    setDueno('')
+    setSintomas('')
+  }
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Fomulario</h2>
       <p className="text-lg mt-5 mb-10">Añade Pacientes y {''}
         <span className="font-bold text-indigo-600">Administralos</span>
       </p>
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
         <div className="mb-5">
-          <label htmlFor="" className="block text-gray-700 uppercase font-bold">Nombre Paciente</label>
-          <input type="text" placeholder="Nombre del animal" className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-500" />
+          <label htmlFor="" className="block text-gray-700 uppercase font-bold">Nombre Paciente: {nombre}</label>
+          <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} placeholder="Nombre del animal" className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-500" />
         </div>
         <div className="mb-5">
           <label htmlFor="" className="block text-gray-700 uppercase font-bold">Nombre Dueño</label>
-          <input type="text" placeholder="Nombre del animal" className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-500" />
+          <input type="text" value={dueno} onChange={(e) => setDueno(e.target.value)} placeholder="Nombre del propietario" className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-500" />
         </div>
         <div className="mb-5">
           <label htmlFor="" className="block text-gray-700 uppercase font-bold">Sintomas</label>
-          <textarea name="" className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-500" />
+          <textarea name="" value={sintoma} onChange={(e) => setSintomas(e.target.value)} className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-500" />
         </div>
         <input type="submit" value="Agregar" className="w-full rounded-md bg-indigo-600 p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors" />
       </form>
--- pacientes/patient_tracker/src/components/Pacientes.jsx
+++ pacientes/patient_tracker/src/components/Pacientes.jsx
@@ -1,18 +1,20 @@
-const Pacientes = () => {
+const Pacientes = ({ paciente }) => {
+  const { nombre, dueno, sintoma } = paciente
   return (
     <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
-        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
-          <span className="font-normal normal-case ">Hook</span>
-        </p>
-        <p className="font-bold mb-3 text-gray-700 uppercase">Dueño: {''}
-          <span className="font-normal normal-case ">Jhon</span>
-        </p>
-        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
-          <span className="font-normal normal-case ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aperiam veritatis molestias tempora eos repudiandae, ea, suscipit, earum quos incidunt totam beatae adipisci. Repudiandae dignissimos voluptate earum laboriosam labore repellat!</span>
-        </p>
-        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
-          <span className="font-normal normal-case ">Hook</span>
-        </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+        <span className="font-normal normal-case ">{nombre}</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Dueño: {''}
+        <span className="font-normal normal-case ">{dueno}</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+        <span className="font-normal normal-case ">{sintoma}</span>
+      </p>
+      <div className="flex justify-between mt-10">
+        <button type="button" className="bg-indigo-600 hover:bg-indigo-700 py-2 px-10 uppercase font-bold rounded-lg" >Editar</button>
+        <button type="button" className="bg-red-600 hover:bg-red-700 py-2 px-10 uppercase font-bold rounded-lg" >Eliminar</button>
+      </div>
     </div>
   )
 }
